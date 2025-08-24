4fbcee11779d8d1d63d2d972060fca9155c17add
JaviRute <jrute@live.com>
2025-08-24 21:55:13 +0100

    tidy year 7 vocab

--- src/components/Expression.js
+++ src/components/Expression.js
@@ -6,19 +6,19 @@ export default function Expression({ targetExpression, spToEngMode }) {
   if (!Array.isArray(targetExpression) || targetExpression.length === 0) {
     return <div>No expressions provided.</div>;
   }
-
+  //Para hacer q salgan todas las expresiones a traducir y no solo una borra el último [0] de los dos targetExpression de abajo. Y luego descomenta las 3 lineas q hay abajo
   // Extract Spanish expressions
-  const spanishExpressions = spToEngMode ? targetExpression[0] : targetExpression[1];
+  const spanishExpressions = spToEngMode ? targetExpression[0][0] : targetExpression[1][0];
 
   // Join Spanish expressions with ", " if there are multiple
-  const expressionString = spanishExpressions.map((expr, index) => 
-    index > 0 ? `, ${expr}` : expr
-  ).join('');
+  // const expressionString = spanishExpressions.map((expr, index) => 
+  //   index > 0 ? `, ${expr}` : expr
+  // ).join('');
 
   return (
     <div>
       <h3 className='target-expression'>
-        Expression to translate: {expressionString}
+        Expression to translate: {spanishExpressions}
       </h3>
     </div>
   );
